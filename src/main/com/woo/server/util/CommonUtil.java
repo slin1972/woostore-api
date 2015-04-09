9a2505759eb62937b460cd6e946fd7818fb41c6f
slin <lykin_1972@163.com>
2015-04-09 17:45:59 +0800

    删除base64util

--- src/main/com/woo/server/util/CommonUtil.java
+++ src/main/com/woo/server/util/CommonUtil.java
@@ -8,9 +8,6 @@ import java.util.Random;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
-
 public class CommonUtil {
 
 	private final static Logger LOG = LoggerFactory.getLogger(CommonUtil.class);  
@@ -63,25 +60,5 @@ public class CommonUtil {
 					3));
 		}
 		return sb.toString();
-	}
-	 /**    
-     * BASE64解密   
-   * @param key          
-     * @return          
-     * @throws Exception          
-     */              
-    public static byte[] decryptBASE64(String key) throws Exception {               
-        return (new BASE64Decoder()).decodeBuffer(key);               
-    }               
-                  
-    /**         
-     * BASE64加密   
-   * @param key          
-     * @return          
-     * @throws Exception          
-     */              
-    public static String encryptBASE64(byte[] key) throws Exception {               
-        return (new BASE64Encoder()).encodeBuffer(key);               
-    }       
-        
+	} 
 }
